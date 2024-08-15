760b0d8988d42c471ca9ff4108f4cf9f42be88bd
Ricardo Montañana Gómez <rmontanana@gmail.com>
2024-08-15 09:59:52 +0200

    Change version read in config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,16 +6,12 @@
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
 import sys
+from pathlib import Path
 
-sys.path.insert(0, os.path.abspath("../../stree/"))
+sys.path.insert(0, str(Path(__file__).parent.parent))
 
-from stree._version import __version__
+import stree
 
 # -- Project information -----------------------------------------------------
 
@@ -24,9 +20,7 @@ copyright = "2020 - 2024, Ricardo Montañana Gómez"
 author = "Ricardo Montañana Gómez"
 
 # The full version, including alpha/beta/rc tags
-version = __version__
-release = version
-
+version = release = stree.__version__
 
 # -- General configuration ---------------------------------------------------
 
